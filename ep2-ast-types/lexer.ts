79026b83d8f3f743122d494baa6a7a9c9ad22500
tylerlaceby <tylerbadgertutorials@gmail.com>
2022-10-24 21:54:53 -0600

    Lesson 3

--- ep2-ast-types/lexer.ts
+++ ep2-ast-types/lexer.ts
@@ -6,59 +6,59 @@
 
 // Represents tokens that our language understands in parsing.
 export enum TokenType {
-	// Literal Types
-	Number,
-	Identifier,
+  // Literal Types
+  Number,
+  Identifier,
 
-	// Keywords
-	Let,
+  // Keywords
+  Let,
 
-	// Grouping * Operators
-	BinaryOperator,
-	Equals,
-	OpenParen,
-	CloseParen,
+  // Grouping * Operators
+  BinaryOperator,
+  Equals,
+  OpenParen,
+  CloseParen,
 }
 
 /**
  * Constant lookup for keywords and known identifiers + symbols.
  */
 const KEYWORDS: Record<string, TokenType> = {
-	let: TokenType.Let,
+  let: TokenType.Let,
 };
 
 // Reoresents a single token from the source-code.
 export interface Token {
-	value: string; // contains the raw value as seen inside the source code.
-	type: TokenType; // tagged structure.
+  value: string; // contains the raw value as seen inside the source code.
+  type: TokenType; // tagged structure.
 }
 
 // Returns a token of a given type and value
 function token(value = "", type: TokenType): Token {
-	return { value, type };
+  return { value, type };
 }
 
 /**
  * Returns whether the character passed in alphabetic -> [a-zA-Z]
  */
 function isalpha(src: string) {
-	return src.toUpperCase() != src.toLowerCase();
+  return src.toUpperCase() != src.toLowerCase();
 }
 
 /**
  * Returns true if the character is whitespace like -> [\s, \t, \n]
  */
 function isskippable(str: string) {
-	return str == " " || str == "\n" || str == "\t";
+  return str == " " || str == "\n" || str == "\t";
 }
 
 /**
  Return whether the character is a valid integer -> [0-9]
  */
 function isint(str: string) {
-	const c = str.charCodeAt(0);
-	const bounds = ["0".charCodeAt(0), "9".charCodeAt(0)];
-	return c >= bounds[0] && c <= bounds[1];
+  const c = str.charCodeAt(0);
+  const bounds = ["0".charCodeAt(0), "9".charCodeAt(0)];
+  return c >= bounds[0] && c <= bounds[1];
 }
 
 /**
@@ -69,65 +69,65 @@ function isint(str: string) {
  * - Does not modify the incoming string.
  */
 export function tokenize(sourceCode: string): Token[] {
-	const tokens = new Array<Token>();
-	const src = sourceCode.split("");
+  const tokens = new Array<Token>();
+  const src = sourceCode.split("");
 
-	// produce tokens until the EOF is reached.
-	while (src.length > 0) {
-		// BEGIN PARSING ONE CHARACTER TOKENS
-		if (src[0] == "(") {
-			tokens.push(token(src.shift(), TokenType.OpenParen));
-		} else if (src[0] == ")") {
-			tokens.push(token(src.shift(), TokenType.CloseParen));
-		} // HANDLE BINARY OPERATORS
-		else if (src[0] == "+" || src[0] == "-" || src[0] == "*" || src[0] == "/") {
-			tokens.push(token(src.shift(), TokenType.BinaryOperator));
-		} // Handle Conditional & Assignment Tokens
-		else if (src[0] == "=") {
-			tokens.push(token(src.shift(), TokenType.Equals));
-		} // HANDLE MULTICHARACTER KEYWORDS, TOKENS, IDENTIFIERS ETC...
-		else {
-			// Handle numeric literals -> Integers
-			if (isint(src[0])) {
-				let num = "";
-				while (src.length > 0 && isint(src[0])) {
-					num += src.shift();
-				}
+  // produce tokens until the EOF is reached.
+  while (src.length > 0) {
+    // BEGIN PARSING ONE CHARACTER TOKENS
+    if (src[0] == "(") {
+      tokens.push(token(src.shift(), TokenType.OpenParen));
+    } else if (src[0] == ")") {
+      tokens.push(token(src.shift(), TokenType.CloseParen));
+    } // HANDLE BINARY OPERATORS
+    else if (src[0] == "+" || src[0] == "-" || src[0] == "*" || src[0] == "/") {
+      tokens.push(token(src.shift(), TokenType.BinaryOperator));
+    } // Handle Conditional & Assignment Tokens
+    else if (src[0] == "=") {
+      tokens.push(token(src.shift(), TokenType.Equals));
+    } // HANDLE MULTICHARACTER KEYWORDS, TOKENS, IDENTIFIERS ETC...
+    else {
+      // Handle numeric literals -> Integers
+      if (isint(src[0])) {
+        let num = "";
+        while (src.length > 0 && isint(src[0])) {
+          num += src.shift();
+        }
 
-				// append new numeric token.
-				tokens.push(token(num, TokenType.Number));
-			} // Handle Identifier & Keyword Tokens.
-			else if (isalpha(src[0])) {
-				let ident = "";
-				while (src.length > 0 && isalpha(src[0])) {
-					ident += src.shift();
-				}
+        // append new numeric token.
+        tokens.push(token(num, TokenType.Number));
+      } // Handle Identifier & Keyword Tokens.
+      else if (isalpha(src[0])) {
+        let ident = "";
+        while (src.length > 0 && isalpha(src[0])) {
+          ident += src.shift();
+        }
 
-				// CHECK FOR RESERVED KEYWORDS
-				const reserved = KEYWORDS[ident];
-				// If value is not undefined then the identifier is
-				// reconized keyword
-				if (reserved) {
-					tokens.push(token(ident, reserved));
-				} else {
-					// Unreconized name must mean user defined symbol.
-					tokens.push(token(ident, TokenType.Identifier));
-				}
-			} else if (isskippable(src[0])) {
-				// Skip uneeded chars.
-				src.shift();
-			} // Handle unreconized characters.
-			// TODO: Impliment better errors and error recovery.
-			else {
-				console.error(
-					"Unreconized character found in source: ",
-					src[0].charCodeAt(0),
-					src[0]
-				);
-				Deno.exit(1);
-			}
-		}
-	}
+        // CHECK FOR RESERVED KEYWORDS
+        const reserved = KEYWORDS[ident];
+        // If value is not undefined then the identifier is
+        // reconized keyword
+        if (reserved) {
+          tokens.push(token(ident, reserved));
+        } else {
+          // Unreconized name must mean user defined symbol.
+          tokens.push(token(ident, TokenType.Identifier));
+        }
+      } else if (isskippable(src[0])) {
+        // Skip uneeded chars.
+        src.shift();
+      } // Handle unreconized characters.
+      // TODO: Impliment better errors and error recovery.
+      else {
+        console.error(
+          "Unreconized character found in source: ",
+          src[0].charCodeAt(0),
+          src[0],
+        );
+        Deno.exit(1);
+      }
+    }
+  }
 
-	return tokens;
+  return tokens;
 }
