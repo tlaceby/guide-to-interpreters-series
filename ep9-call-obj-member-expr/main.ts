29701c81167d196423ef0efe0e94981a9a7726bd
Tyler Laceby <31893030+tylerbadger23@users.noreply.github.com>
2023-01-05 10:29:40 -0700

    ready for episode on native builtin functions

--- ep9-call-obj-member-expr/main.ts
+++ ep9-call-obj-member-expr/main.ts
@@ -1,39 +1,16 @@
 import Parser from "./frontend/parser.ts";
-import Environment, { createGlobalEnv } from "./runtime/environment.ts";
+import { createGlobalEnv } from "./runtime/environment.ts";
 import { evaluate } from "./runtime/interpreter.ts";
 
-// repl();
 run("./test.txt");
 
 async function run(filename: string) {
-  const parser = new Parser();
-  const env = createGlobalEnv();
+	const parser = new Parser();
+	const env = createGlobalEnv();
 
-  const input = await Deno.readTextFile(filename);
-  const program = parser.produceAST(input);
+	const input = await Deno.readTextFile(filename);
+	const program = parser.produceAST(input);
 
-  const result = evaluate(program, env);
-  console.log(result);
-}
-
-function repl() {
-  const parser = new Parser();
-  const env = createGlobalEnv();
-  // INITIALIZE REPL
-  console.log("\nRepl v0.1");
-
-  // Continue Repl Until User Stops Or Types `exit`
-  while (true) {
-    const input = prompt("> ");
-    // Check for no user input or exit keyword.
-    if (!input || input.includes("exit")) {
-      Deno.exit(1);
-    }
-
-    // Produce AST From sourc-code
-    const program = parser.produceAST(input);
-
-    const result = evaluate(program, env);
-    console.log(result);
-  }
+	const result = evaluate(program, env);
+	console.log(result);
 }
